32fface2310ae242bd35df2b6d27d7b22be78e0b
Trinity Terry <trinityterry@Trinitys-MacBook-Air.local>
2018-01-15 19:29:30 -0600

    1/15/18

---
+++ class-four-mini-two.js
@@ -0,0 +1,6 @@
+// 1. Write (declare) a function called tellMyUserTheyAreCool that has one parameter: the user's name. This function should console log "Hey [user's name], I heard you were pretty cool."
+function tellMyUserTheyAreCool(userName) {
+    console.log(`Hey ${userName}, I heard you were pretty cool.`)
+  }
+  // 2. Call the function tellMyUserTheyAreCool and pass in the name of your favorite friend.
+  tellMyUserTheyAreCool("luna");
